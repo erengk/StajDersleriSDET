f9235e2084871da5ca4139addb95c271af671ef1
gokhanerenkamer <gek@ogr.eskisehir.edu.tr>
2023-08-04 09:50:22 +0300

    Scroll ile birlikte windows geçişleri düzeltildi

--- src/main/java/Ders7/_03_Ders.java
+++ src/main/java/Ders7/_03_Ders.java
@@ -4,33 +4,55 @@ import Utility.BaseDriver;
 import Utility.MyFunctions;
 import org.junit.Test;
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebElement;
-
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import java.time.Duration;
 import java.util.List;
 import java.util.Set;
 
 public class _03_Ders extends BaseDriver {
     @Test
     public void Test1() {
-        driver.get("https://www.selenium.dev/downloads/");
+        driver.get("https://www.selenium.dev/");
+        JavascriptExecutor js = (JavascriptExecutor) driver;
 
-        String anasayfaWindowID = driver.getWindowHandle();
-        System.out.println("anasayfaWindowID = " + anasayfaWindowID);
+        MyFunctions.Bekle(2);
+        String anaSayfaWindowId = driver.getWindowHandle();
+        System.out.println("anaSayfaWindowId = " + anaSayfaWindowId);
+        List<WebElement> linkler = driver.findElements(By.cssSelector("a[target='_blank']"));
 
-        List<WebElement> aLinkBlank = driver.findElements(By.cssSelector("a[target='_blank']"));
-
-        for (WebElement l : aLinkBlank) {
-            //System.out.println("l.getAttribute(\"href\") = " + l.getAttribute("href"));
-            if (!l.getAttribute("href").contains("mail")) {
-                l.click();
+        // Aşağıdaki şu anda bütün sayfalar açıldı
+        for (WebElement link : linkler) {
+            if (!link.getAttribute("href").contains("mail")) {
+                WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(30));
+                wait.until(ExpectedConditions.elementToBeClickable(link));
+                js.executeScript("arguments[0].scrollIntoView(true);", link);
+                js.executeScript("arguments[0].click();", link);
+                driver.switchTo().window(anaSayfaWindowId);
                 MyFunctions.Bekle(2);
-                driver.switchTo().window(anasayfaWindowID);
             }
         }
-        Set<String> windowsID = driver.getWindowHandles();
-        for (String id: windowsID) {
-            System.out.println("id = " + id);
+
+// her bir sayfaya geçip url ve title ını yazdırma
+        Set<String> windowsIdLer = driver.getWindowHandles();
+        for (String id : windowsIdLer) {
+            MyFunctions.Bekle(2);
+            driver.switchTo().window(id); // sıradaki tabdaki window a geçmiş oldum.
+            System.out.println("title=" + driver.getTitle() + ", url=" + driver.getCurrentUrl());
         }
+
+// ana sayfa harici kapama
+        for (String id : windowsIdLer) {
+            if (id.equals(anaSayfaWindowId))
+                continue;
+
+            driver.switchTo().window(id); // sıradaki tabdaki window a geçmiş oldum.
+            driver.close();
+        }
+
+
         BekleKapat();
     }
 }
